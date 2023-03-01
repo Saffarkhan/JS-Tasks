d216edb17f9f2f5a1a822b67fe77f7b18e41d958
Saffar Khan <saffar.khan98@gmail.com>
2023-03-01 11:13:09 +0500

    Task-2: Return Number of True Values in Array

--- task.js
+++ task.js
@@ -1,13 +1,7 @@
-//Question-1
-/* * Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-It should remove all values from list a, which are present in list b keeping their order.arrayDiff([1,2],[1]) == [2]
-If a value is present in b, all of its occurrences must be removed from the other: arrayDiff([1,2,2,2,3],[2]) == [1,3]
-*/
-//Solution: 
-export function arrayDiff(a, b) {
-    return a.filter(function (x) {
-        return !b.includes(x)
-    })
-    return a.filter((x) => !b.includes(x))
+/* Create a function which returns the number of true values there are in an array.*/
+
+//Solution:
+export function countTrue(array) {
+    return array.filter((x) => x === true).reduce((pre, curr) => pre + curr, 0);
 }
-console.log(arrayDiff([1, 2, 2, 2, 3], [2]))
+console.log(countTrue([true, false, true, true, false]))
